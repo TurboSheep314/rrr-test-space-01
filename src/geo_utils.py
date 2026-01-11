7154e6fdabc0f78919a5b04fc016b6b39dbf8de3
TurboSheep314 <51458648+TurboSheep314@users.noreply.github.com>
2026-01-11 07:23:52 -0500

    updated geoutils for pushing to streamlit, added app.py

--- src/geo_utils.py
+++ src/geo_utils.py
@@ -1,7 +1,22 @@
 import geopandas as gpd
 
-def load_zip_shapes(path: str, zip_col="ZCTA5CE10"):
+# def load_zip_shapes(path: str, zip_col="ZCTA5CE10"):
+#     gdf = gpd.read_file(path)
+
+#     gdf["Zip"] = gdf[zip_col].astype(str).str.zfill(5)
+#     return gdf[["Zip", "geometry"]]
+
+
+def load_zip_shapes(path: str):
     gdf = gpd.read_file(path)
+
+    # Auto-detect ZIP/ZCTA field across Census years/products
+    for candidate in ["ZCTA5CE20", "ZCTA5CE10", "GEOID20", "GEOID"]:
+        if candidate in gdf.columns:
+            zip_col = candidate
+            break
+    else:
+        raise ValueError(f"No ZIP/ZCTA column found. Columns: {list(gdf.columns)}")
 
     gdf["Zip"] = gdf[zip_col].astype(str).str.zfill(5)
     return gdf[["Zip", "geometry"]]

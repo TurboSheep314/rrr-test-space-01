e2f003d64a20dcc486447bb917c3393b054b1b9b
TurboSheep314 <51458648+TurboSheep314@users.noreply.github.com>
2026-01-15 13:24:57 -0500

    added hover data function, added reset button

--- src/heatmap.py
+++ src/heatmap.py
@@ -1,5 +1,6 @@
 import folium
 from folium.plugins import HeatMap
+from branca.element import Template, MacroElement
 
 def create_zip_heatmap(gdf, value_col, center=(42.3, -71.1), zoom=9):
     m = folium.Map(location=center, zoom_start=zoom, tiles="cartodbpositron")
@@ -20,5 +21,29 @@ def create_zip_heatmap(gdf, value_col, center=(42.3, -71.1), zoom=9):
         blur=18,
         min_opacity=0.4
     ).add_to(m)
+
+    folium.GeoJson(
+    gdf,
+    style_function=lambda feature: {"fillOpacity": 0},
+    tooltip=folium.GeoJsonTooltip(
+        fields=["Zip", "Composite Score"],
+        aliases=["ZIP Code", "Composite Score"],
+        localize=True
+    ),
+    ).add_to(m)
+    
+    
+
+    template = """
+    {% macro html(this,kwargs) %}
+    <div style="position: fixed; bottom: 50px; left: 50px; z-index:9999; background-color:white; padding:10px;">
+        <b>Composite Score</b><br>
+        Hotter = Higher Score
+    </div>
+    {% endmacro %}
+    """
+    m.get_root().add_child(MacroElement().add_child(Template(template)))
+
+   
 
     return m
